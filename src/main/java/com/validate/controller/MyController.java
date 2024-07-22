368f6931e7134a5d1bb7ad72303f89e5f417300d
Vijay Bighane <124725301+vijaybighane@users.noreply.github.com>
2024-07-22 23:18:05 +0530

    Update MyController.java

--- src/main/java/com/validate/controller/MyController.java
+++ src/main/java/com/validate/controller/MyController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-
+// this is the controller method
 import com.validate.entities.LoginData;
 
 @Controller
